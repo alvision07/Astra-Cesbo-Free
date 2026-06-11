920d0055edac52d18fe866934ba2c70876b2937b
schqvho <alvision07@gmail.com>
2026-06-12 03:14:16 +0900

    Update astra_install.sh

--- astra_install.sh
+++ astra_install.sh
@@ -4,6 +4,6 @@ wget -q https://cdn.cesbo.com/astra/4.4.182-free/x86/linux-64bit/astra > /dev/nu
 chmod +x astra > /dev/null
 sudo mv astra /usr/bin/ > /dev/null
 sudo mkdir /etc/astra > /dev/null
-echo "installation was succesful...Please Wait ..."
+echo "installation was succesful Please Wait "
 echo "Setting (Port 5555)"
 echo "astra -c /etc/astra/iptv.conf -p 5555"
